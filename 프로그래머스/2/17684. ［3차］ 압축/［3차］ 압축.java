dd7c902e36174bfa2441cc1928ee8dbe3becf6eb
Yerin Park <60564216+parfait2@users.noreply.github.com>
2024-01-25 17:33:28 +0900

    [level 2] Title: [3차] 압축, Time: 1.40 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,65 +1,59 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 class Solution {
     public ArrayList<Integer> solution(String msg) {
-        
+        // 문자열을 압축한 후의 사전 색인 번호
         ArrayList<Integer> answer = new ArrayList<>();
 
         // 사전
-        ArrayList<String> alphabet = new ArrayList<>();
+        HashMap<String, Integer> alphabet = new HashMap<>();
 
         // 사전 초기화
         for(int i = 0; i < 26; i++) {
-            alphabet.add(Character.toString((char) (i + 65)));
+            alphabet.put(Character.toString((char) (i + 65)), i + 1);
         }
 
         int remain = 0;
 
         for(int i = 0; i < msg.length(); i++) {
-            String w = "";
-            String str = "";
+            StringBuilder w = new StringBuilder();
+            StringBuilder str = new StringBuilder();
 
             // 현재 인덱스에 해당하는 문자를 한 글자를 str 담는다.
-            str += msg.charAt(i);
+            str.append(msg.charAt(i));
 
             for(int j = i + 1; j < msg.length(); j++) {
                 // str을 w에 저장한다.
-                w = str;
+                w.setLength(0);
+                w.append(str);
 
                 // str에 그 다음 한 글자를 더 붙인다.
-                str += msg.charAt(j);
+                str.append(msg.charAt(j));
 
-                if(!alphabet.contains(str)) { // str이 새로운 단어이며,
+                if (!alphabet.containsKey(str.toString())) { // str이 새로운 단어라면
+                    alphabet.put(str.toString(), alphabet.size() + 1); // 사전에 str을 등록한다.
 
-                    alphabet.add(str); // 사전에 str을 등록한다.
-
-                    /* 중요 */
                     if(w.length() > 1) i += w.length() - 1; // w이 두 글자 이상이면 i 인덱스가 (글자 길이 - 1)에 가게 한다.
 
                     remain = i; // i 인덱스를 remain에 잠시 저장한다. (마지막으로 처리되지 않은 글자)
 
-                    answer.add(alphabet.indexOf(w) + 1); // 사전에서 w의 색인 번호를 찾아 정답 배열에 담는다.
+                    answer.add(alphabet.get(w.toString())); // 사전에서 w의 색인 번호를 찾아 정답 배열에 담는다.
 
                     break;
                 }
             }
         }
 
-        String lastStr = "";
-
         // 입력이 길이가 1인 문자열일 때
         if(msg.length() == 1) {
             answer.add(1);
-        } else {
-            // 길이가 2 이상의 문자열은 위의 로직을 거치고 옴
-            // 마지막 남은 글자 answer에 추가
-            for(int i = remain + 1; i < msg.length(); i++) {
-                lastStr += msg.charAt(i);
-            }
+        }
 
-            // 새로운 단어는 위의 로직에서 모두 등록을 마쳤기 때문에
-            // lastStr은 무조건 사전에 등록된 단어이다.
-            answer.add(alphabet.indexOf(lastStr) + 1);
+        // 마지막 글자 처리
+        if (remain < msg.length() - 1) {
+            String lastStr = msg.substring(remain + 1);
+            answer.add(alphabet.get(lastStr));
         }
 
         return answer;
